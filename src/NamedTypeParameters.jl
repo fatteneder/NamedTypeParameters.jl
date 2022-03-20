a9b22fef7c53f1909e68e244effa8c43b8023965
Florian Atteneder <florian.atteneder@gmail.com>
2022-03-20 11:03:30 +0100

    add checks, functionality and tests

--- src/NamedTypeParameters.jl
+++ src/NamedTypeParameters.jl
@@ -1,21 +1,47 @@
 module NamedTypeParameters
 
 
-export get_type_signature,
-       parameterize,
-       @parameterize
+export parameterize,
+       @parameterize,
+       expand_type_parameters
 
 
-get_type_signature(ua::UnionAll) = get_type_signature(ua.body)
-get_type_signature(dt::DataType) = (dt.name, dt.parameters)
+expand_type_parameters(x::Union{DataType,UnionAll}) = Base.unwrap_unionall(x)
+
+
+function parse_overrides(typeparameters)
+  list_name, list_type, list_isdummy = [], [], []
+  for tp in typeparameters
+    name, type, isdummy = if tp isa Symbol
+      # {..., A, ...} -> {..., <:default_supertype_of_A, ...}
+      tp, nothing, false
+    elseif tp.head === :<:         
+      # {..., A <: SuperType, ...} -> {..., <:SuperType, ...}
+      tp.args[1], tp.args[2], false
+    elseif tp.head === :call && tp.args[1] === :(=>)
+      # {..., A => T, ...} -> {..., T, ...}
+      tp.args[2], tp.args[3], true
+    elseif tp.head === :(=)
+      # {..., A = Float64, ...} -> {..., Float64, ...}
+      tp.args[1], tp.args[2], false
+    else
+      error("Unrecognized type pattern $(tp)")
+    end
+    push!(list_name, name)
+    push!(list_type, type)
+    push!(list_isdummy, isdummy)
+  end
+  return list_name, list_type, list_isdummy
+end
 
 
 function parameterize(type, override_parameters=[])
-  typename, typeparameters = get_type_signature(type)
+  fulltype = expand_type_parameters(type)
+  typename, typeparameters = fulltype.name, fulltype.parameters
   if length(typeparameters) < length(override_parameters)
-    error("Requested too many parameters for type $type")
+    error("Too many parameters for type '$type'")
   end
-  if length(typeparameters) == 0
+  if length(typeparameters) == -1
     return quote 
       $(esc(typename.name)) 
     end
@@ -23,28 +49,26 @@ function parameterize(type, override_parameters=[])
   # parameter_list = [ Expr(:<:, Symbol(p.name), Symbol(p.ub)) for p in typeparameters ]
   parameter_list = Union{Expr,Symbol}[ Expr(:<:, Symbol(p.ub)) for p in typeparameters ]
   parameter_names = [ p.name for p in typeparameters ]
+
+  override_name, override_type, override_isdummy = parse_overrides(override_parameters)
+  if length(unique(override_name)) != length(override_name)
+    error("Duplicated parameter names")
+  end
+
   # override
-  # TODO Check for duplicates in override_parameters
-  for override in override_parameters
-    name, type, type_is_dummy = if override isa Symbol
-      override, nothing, false
-    elseif override.head === :<:         
-      override.args[1], override.args[2], false
-    elseif override.head === :(=)
-      override.args[1], override.args[2], true
-    else
-      error("Unrecognized type pattern $(override)")
-    end
+  for (name, type, isdummy) in zip(override_name, override_type, override_isdummy)
     # parameter names must be unique, hence, findfirst is enough
     index = findfirst(n -> n == name, parameter_names)
-    isnothing(index) && continue
+    if isnothing(index)
+      error("Unknown parameter name '$name'")
+    end
     # parameter_list[index] = Expr(:<:, Symbol(name), Symbol(type))
     # parameter_list[index] = Expr(:<:, Symbol(type))
-    isnothing(type) && continue # typename without a supertype or assignment falls back to default
-    if !type_is_dummy
-      parameter_list[index] = Expr(:<:, Symbol(type))
-    else
+    isnothing(type) && continue # typename without a supertype falls back to default
+    if isdummy
       parameter_list[index] = type
+    else
+      parameter_list[index] = Expr(:<:, Symbol(type))
     end
   end
   new_signature = Expr(:curly, typename.name, parameter_list...)
@@ -56,31 +80,35 @@ end
 
 
 """
-    parameterize(short_signature)
+    parameterize(signature)
 
 ===
 # Examples
 
-struct MyType{A,B,C,D} end
-
-@parameterize MyType{B<:Float64, C, D=T}
+```
+struct MyType{A<:Real,B,C,D} end
+@parameterize MyType{D=T, B<:Float64, C}
+```
 transforms into
-MyType{<:Any, <:Float64, <:Any, T}
-Here is what happend for each slot:
-(A) We inserted <:Any, because we did not specify anything for slot A and <:Any is its default supertype
-(as given in the definion of MyType).
-(B) We inserted <:Float64 as requested.
-(C) We inserted <: Any, because although it was mentioned, no supertype was provided.
-(D) We inserted T as a placeholder for a preceeding `where` list.
+```
+MyType{<:Real, <:Float64, <:Any, T}
+```
+
+Here is what happend for each slot (the order inside @parameterize does not matter)
+- (A) We inserted <:Real, because we did not specify anything for slot A and <:Real is its 
+default supertype (according to the definion of MyType).
+- (B) We inserted <:Float64 as requested.
+- (C) We inserted <:Any, because although it was skipped, no supertype was provided.
+- (D) We inserted T as a placeholder for a preceeding `where` phrase (must be added separately).
 """
-macro parameterize(short_signature)
-  if short_signature isa Symbol
-    return esc(short_signature)
+macro parameterize(signature)
+  if signature isa Symbol
+    return esc(signature)
   end
-  if short_signature.head !== :curly
-    error("Could not recognize a type")
+  if signature.head !== :curly
+    error("Failed to recognize a type in '$signature'")
   end
-  type, override_parameters = short_signature.args[1], short_signature.args[2:end]
+  type, override_parameters = signature.args[1], signature.args[2:end]
   type = @eval __module__ $type
   return esc(parameterize(type, override_parameters))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,6 @@ struct NoTypes end
 struct AnyTypes1{A} end
 struct AnyTypes2{A,B} end
 struct AnyTypes3{A,B,C} end
-# struct SuperTypes1{A<:Real} end
-# struct SuperTypes2{A<:Real,B<:String} end
-# struct SuperTypes3{A<:Real,B<:String,C<:Symbol} end
 struct SuperTypes1{A<:Real} end
 struct SuperTypes2{A<:Real,B<:Array} end
 struct SuperTypes3{A<:Real,B<:Array,C<:AbstractDict} end
@@ -23,17 +20,32 @@ for t in types
 end
 
 
-# override defaults
+### Notes parameter types
+
+# specifying a type with <: equals adding an unnamed where parameter with <: Real
+@test SuperTypes1{<:Real} == SuperTypes1{A} where A<:Real
+# note: using invalid where supertypes (invalid wrt the definition of the parameteric type)
+# does not throw
+SuperTypes1{A} where A<:String # works, although SuperTypes1 is defined with A<:Real
+# but inserting an invalid type as a concrete type parameter does throw
+@test_throws TypeError SuperTypes1{String}
+
+
+# using the parameter type name alone inserts its default supertype
+@test @parameterize(AnyTypes1{A}) == AnyTypes1{<:Any}
+@test @parameterize(AnyTypes2{A,B}) == AnyTypes2{<:Any, <:Any}
+@test @parameterize(AnyTypes3{A,B,C}) == AnyTypes3{<:Any, <:Any, <:Any}
+@test @parameterize(SuperTypes1{A}) == SuperTypes1{<:Real}
+@test @parameterize(SuperTypes2{A,B}) == SuperTypes2{<:Real, <:Array}
+@test @parameterize(SuperTypes3{A,B,C}) == SuperTypes3{<:Real, <:Array, <:AbstractDict}
+
+# override type parameter defaults
 @test @parameterize(AnyTypes1{A<:Float64}) == AnyTypes1{<:Float64}
 @test @parameterize(AnyTypes2{A<:Float64,B<:String}) == AnyTypes2{<:Float64,<:String}
 @test @parameterize(AnyTypes3{A<:Float64,B<:String,C<:Symbol}) == AnyTypes3{<:Float64,<:String,<:Symbol}
 @test @parameterize(SuperTypes1{A<:Float64}) == SuperTypes1{<:Float64}
 @test @parameterize(SuperTypes2{A<:Float64,B<:Vector}) == SuperTypes2{<:Float64,<:Vector}
 @test @parameterize(SuperTypes3{A<:Float64,B<:Vector,C<:Dict}) == SuperTypes3{<:Float64,<:Vector,<:Dict}
-# note that inserting an invalid supertype for a type parameter does not throw
-SuperTypes1{<:String} # valid, although defined as SuperTypes{A<:Real}
-# but inserting an invalid type for a type parameter does
-@test_throws TypeError SuperTypes1{String}
 
 # skip parameters
 @test @parameterize(AnyTypes2{A<:Float64}) == AnyTypes2{<:Float64,<:Any}
@@ -64,12 +76,52 @@ SuperTypes1{<:String} # valid, although defined as SuperTypes{A<:Real}
 @test_throws LoadError @eval @parameterize(SuperTypes3{A,B,C,D})
 
 
-# parameterize(NoTypes)
-# # parameterize(AnyTypes1)
-# # parameterize(AnyTypes1, [(:A, :Float64)])
-# parameterize(AnyTypes2, [Expr(:<:, :B, :Float64)])
-# # @test_throws LoadError parameterize(AnyTypes2, [Expr(:curly, :B, :Float64)])
+# name=dummy inserts the dummy in place of name, to be used with where syntax
+# note that AnyTypes1{<:Any} == AnyTypes1{T} where <: Any
+@test (@parameterize(AnyTypes1{A=>T}) where {T<:Real}) == AnyTypes1{<:Real}
+@test (@parameterize(AnyTypes2{A=>T,B=>S}) where {T<:Real,S<:Array}) == AnyTypes2{<:Real, <:Array}
+@test (@parameterize(AnyTypes3{A=>T,B=>S,C=>R}) where {T<:Real,S<:Array,R<:Symbol}) == AnyTypes3{<:Real, <:Array, <:Symbol}
+@test (@parameterize(SuperTypes1{A=>T}) where {T<:Float64}) == SuperTypes1{<:Float64}
+@test (@parameterize(SuperTypes2{A=>T,B=>S}) where {T<:Float64,S<:Vector}) == SuperTypes2{<:Float64, <:Vector}
+@test (@parameterize(SuperTypes3{A=>T,B=>S,C=>R}) where {T<:Float64,S<:Vector,R<:Dict}) == SuperTypes3{<:Float64, <:Vector, <:Dict}
 
-# @parameterize(AnyTypes1, :by, B<:Float64)
-# args = @parameterize AnyTypes3{A, B<:Float64, C=T}
-# args = @parameterize(AnyTypes3{A, B<:Float64, C=T}) where T
+
+# # set a parameter type with =
+# @test @parameterize(AnyTypes1{A=Float64}) == AnyTypes1{Float64}
+# @test @parameterize(AnyTypes2{A=Float64,B=String}) == AnyTypes2{Float64,String}
+# @test @parameterize(AnyTypes3{A=Float64,B=String,C=Symbol}) == AnyTypes3{Float64,String,Symbol}
+# @test @parameterize(SuperTypes1{A=Float64}) == SuperTypes1{Float64}
+# @test @parameterize(SuperTypes2{A=Float64,B=Vector}) == SuperTypes2{Float64,Vector}
+# @test @parameterize(SuperTypes3{A=Float64,B=Vector,C=Dict}) == SuperTypes3{Float64,Vector,Dict}
+
+
+# duplicated parameter names
+@test_throws LoadError @eval @parameterize(AnyTypes2{A,A})
+@test_throws LoadError @eval @parameterize(AnyTypes3{A,A,A})
+@test_throws LoadError @eval @parameterize(AnyTypes3{A,A,C})
+@test_throws LoadError @eval @parameterize(AnyTypes3{A,B,A})
+@test_throws LoadError @eval @parameterize(AnyTypes3{A,B,B})
+@test_throws LoadError @eval @parameterize(SuperTypes2{A,A})
+@test_throws LoadError @eval @parameterize(SuperTypes3{A,A,A})
+@test_throws LoadError @eval @parameterize(SuperTypes3{A,A,C})
+@test_throws LoadError @eval @parameterize(SuperTypes3{A,B,A})
+@test_throws LoadError @eval @parameterize(SuperTypes3{A,B,B})
+
+
+# unknown parameter names
+@test_throws LoadError @eval @parameterize(AnyTypes1{X})
+@test_throws LoadError @eval @parameterize(AnyTypes2{X})
+@test_throws LoadError @eval @parameterize(AnyTypes2{X,Y})
+@test_throws LoadError @eval @parameterize(AnyTypes3{X})
+@test_throws LoadError @eval @parameterize(AnyTypes3{X,Y})
+@test_throws LoadError @eval @parameterize(AnyTypes3{X,Y,Z})
+@test_throws LoadError @eval @parameterize(AnyTypes3{A,Y})
+@test_throws LoadError @eval @parameterize(AnyTypes3{A,B,Z})
+@test_throws LoadError @eval @parameterize(SuperTypes1{X})
+@test_throws LoadError @eval @parameterize(SuperTypes2{X})
+@test_throws LoadError @eval @parameterize(SuperTypes2{X,Y})
+@test_throws LoadError @eval @parameterize(SuperTypes3{X})
+@test_throws LoadError @eval @parameterize(SuperTypes3{X,Y})
+@test_throws LoadError @eval @parameterize(SuperTypes3{X,Y,Z})
+@test_throws LoadError @eval @parameterize(SuperTypes3{A,Y})
+@test_throws LoadError @eval @parameterize(SuperTypes3{A,B,Z})
